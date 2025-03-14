4f3d19ab775902f3cf476bcd52b398e45546b40d
Pedro-Leall <130075532+Pedro-Leall@users.noreply.github.com>
2025-03-13 22:30:15 -0300

    Add files via upload

---
+++ Pesquisa_de_dados_pessoais.cpp
@@ -0,0 +1,21 @@
+/*
+	Name:Dados_pessoais.cpp
+	Author: Pedro Leal
+	Date: 11/03/25 20:24
+	Description: 
+*/
+
+#include <stdio.h>
+
+main(){
+	char nome;
+	int idade;
+	float altura;
+	
+	idade = 0;
+	altura = 0.0;
+	
+	puts("Escreva seu nome completo, Por favor: ");
+	scanf("%s", &nome);
+	printf("%s", nome);
+}
